a3f5e5b36c81b5ce891d324c55e8e3eaaee43d02
rp3595686 <115085184+rp3595686@users.noreply.github.com>
2023-02-02 13:11:38 +0800

    Add SettingsPage

--- lib/conpoments/setting_page.dart
+++ lib/conpoments/setting_page.dart
@@ -1,13 +1,58 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:utifeeder_flutter/conpoments/page_scaffold.dart';
 
-class SettingPage extends StatelessWidget {
+class SettingPage extends StatefulWidget {
+  const SettingPage({super.key});
+  @override
+  State<SettingPage> createState() => _SettingPageState();
+}
+
+int phInterval_temp = 5;
+int phInterval = 5;
+
+class _SettingPageState extends State<SettingPage> {
   @override
   Widget build(BuildContext context) {
     return PageScaffold(
       title: 'Settings',
-      body: Center(
-        child: Text('Settings', style: Theme.of(context).textTheme.headline4),
+      body: SafeArea(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8),
+              child: TextFormField(
+                initialValue: phInterval.toString(),
+                keyboardType: TextInputType.number,
+                inputFormatters: [
+                  FilteringTextInputFormatter.digitsOnly,
+                ],
+                onChanged: (value) {
+                  if (value != '') {
+                    phInterval_temp = int.parse(value);
+                  }
+                },
+                decoration: const InputDecoration(
+                  border: UnderlineInputBorder(),
+                  labelText: 'Fetch pH data intervals',
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8),
+              child: ElevatedButton(
+                onPressed: () {
+                  setState(() {
+                    phInterval = phInterval_temp;
+                  });
+                },
+                child: Text('Save'),
+                style: ElevatedButton.styleFrom(
+                    backgroundColor: Theme.of(context).primaryColorDark),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
