806cb7e2aa2eb1af09b2972145a4457d188343c8
rp3595686 <115085184+rp3595686@users.noreply.github.com>
2023-02-09 14:07:46 +0800

    Merge firebase_fetch.dart into dashboard_page.dart

--- lib/conpoments/dashboard_page.dart
+++ lib/conpoments/dashboard_page.dart
@@ -1,13 +1,15 @@
 import 'dart:async';
+import 'dart:convert';
 
-import 'package:flutter/cupertino.dart';
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:utifeeder_flutter/conpoments/setting_page.dart';
 
-import '../firebase_fetch.dart';
+import '../main.dart';
 import '../responsive.dart';
 import 'graph.dart';
+import 'json_convert.dart';
 import 'page_scaffold.dart';
 
 bool timer_isRunning = true;
@@ -22,6 +24,131 @@ class DashboardPage extends StatefulWidget {
 class _DashboardPageState extends State<DashboardPage> {
   String currentPHLevel = '0', currentTempLevel = '0';
   Timer? timer_ph, timer_temp;
+
+  final firebase_ref = firebaseDB.ref();
+  Future fetchData_ph(Timer timer_ph) async {
+    if (!timer_isRunning) {
+      // cancel the timer
+      timer_ph.cancel();
+    }
+    DataSnapshot snapshot_ph =
+        await firebase_ref.child('ph/').limitToLast(10).get();
+    if (snapshot_ph.exists) {
+      final jsonResponse_ph = json.encode(snapshot_ph.value);
+      final Response_ph = graphdataFromJson(jsonResponse_ph);
+
+      if (phGraphData.isEmpty) {
+        //Add all data
+        for (var data in Response_ph.values) {
+          DateTime time = data.time;
+          double value = data.value;
+          phGraphData.add(GraphData(time: time, value: value));
+          setState(() {});
+          //chartSeriesController_ph?.updateDataSource(
+          //    addedDataIndexes: <int>[phGraphData.length - 1]);
+        }
+      } else {
+        for (var data in Response_ph.values) {
+          DateTime time = data.time;
+          double value = data.value;
+          bool isExist = false;
+
+          //Check if exist
+          for (var a in phGraphData) {
+            if (a.time == time) {
+              isExist = true;
+              break;
+            }
+          }
+          if (isExist == false) {
+            phGraphData.add(GraphData(time: time, value: value));
+            chartSeriesController_ph?.updateDataSource(
+              addedDataIndex: phGraphData.length - 1,
+            );
+          }
+        }
+        if (phGraphData.length > 10) {
+          while (phGraphData.length > 10) {
+            phGraphData.removeAt(0);
+            chartSeriesController_ph?.updateDataSource(
+              removedDataIndex: 0,
+            );
+          }
+        }
+        setState(() {
+          currentPHLevel = phGraphData[phGraphData.length - 1].value.toString();
+        });
+      }
+    }
+  }
+
+  Future fetchData_temp(Timer timer_temp) async {
+    if (!timer_isRunning) {
+      // cancel the timer
+      timer_temp.cancel();
+    }
+    DataSnapshot snapshot_temp =
+        await firebase_ref.child('temp/').limitToLast(10).get();
+    if (snapshot_temp.exists) {
+      final jsonResponse_temp = json.encode(snapshot_temp.value);
+      final Response_temp = graphdataFromJson(jsonResponse_temp);
+
+      if (tempGraphData.isEmpty) {
+        //Add all data
+        for (var data in Response_temp.values) {
+          DateTime time = data.time;
+          double value = data.value;
+          tempGraphData.add(GraphData(time: time, value: value));
+          setState(() {});
+        }
+      } else {
+        for (var data in Response_temp.values) {
+          DateTime time = data.time;
+          double value = data.value;
+          bool isExist = false;
+
+          //Check if exist
+          for (var a in tempGraphData) {
+            if (a.time == time) {
+              isExist = true;
+              break;
+            }
+          }
+          if (isExist == false) {
+            tempGraphData.add(GraphData(time: time, value: value));
+            chartSeriesController_temp?.updateDataSource(
+              addedDataIndex: tempGraphData.length - 1,
+            );
+          }
+          setState(() {
+            currentTempLevel =
+                tempGraphData[tempGraphData.length - 1].value.toString();
+          });
+        }
+      }
+      if (tempGraphData.length > 10) {
+        while (tempGraphData.length > 10) {
+          tempGraphData.removeAt(0);
+          chartSeriesController_temp?.updateDataSource(
+            removedDataIndex: 0,
+          );
+        }
+      }
+    }
+  }
+
+  Future fetchConfig() async {
+    DataSnapshot snapshot_config = await firebase_ref.child('config/').get();
+    if (snapshot_config.exists) {
+      final jsonResponse_config = json.encode(snapshot_config.value);
+
+      var configData = jsonDecode(jsonResponse_config);
+      phInterval = configData['phInterval'];
+      phInterval_temporary = configData['phInterval'].toString();
+      tempInterval = configData['tempInterval'];
+      tempInterval_temporary = configData['tempInterval'].toString();
+    }
+  }
 
   @override
   void initState() {
